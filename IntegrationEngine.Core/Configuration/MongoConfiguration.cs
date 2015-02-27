2a63d342c8f7844b7a3450ebf02df33d1c1d41e5
Ethan Hann <ethanhann@gmail.com>
2015-02-26 20:29:38 -0500

    Add Refactor config files and mongo test

---
+++ IntegrationEngine.Core/Configuration/MongoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace IntegrationEngine.Core.Configuration
+{
+    public class MongoConfiguration : IMongoConfiguration
+    {
+        public string IntegrationPointName { get; set; }
+        public string HostName { get; set; }
+
+        public MongoConfiguration()
+        {}
+
+        public MongoConfiguration(IEngineConfiguration engineConfiguration, string integrationPointName)
+            : this()
+        {
+            var config = engineConfiguration.IntegrationPoints.MongoDB.Single(x => x.IntegrationPointName == integrationPointName);
+            IntegrationPointName = integrationPointName;
+            HostName = config.HostName;
+        }
+    }
+}
---
+++ IntegrationServer.Tests/MongoDBTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeekmanLabs.UnitTesting;
+using NUnit.Framework;
+
+namespace IntegrationServer.Tests
+{
+    public class MongoDBTest
+    {
+        [Test]
+        public void ShouldInitializeEngine()
+        {
+            Program.Start();
+
+            Assert.That(Program.EngineHost, Is.Not.Null);
+
+            Program.Stop();
+        }
+    }
+}
